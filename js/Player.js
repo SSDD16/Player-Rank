f5b1c142be1315bbceb69d16dbed8d99ac133f05
SSDD16 <69150949+SSDD16@users.noreply.github.com>
2021-03-08 18:52:35 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,54 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.rank = null
+    this.name = null;
+
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+getCarAtEnd(){
+  var carRef = database.ref('carAtEnd')
+  carRef.on("value",(data)=>{
+    this.rank = data.val();
+})
+
+  }
+static updatecCar(rank){
+   database.ref('/').update({
+    CarAtEnd: rank
+
+   });
+ }
+
+
+
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
